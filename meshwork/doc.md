566672a66b268b204d8cbb5012e42a5465a2074b
miromarchi <miro.marchi@gmail.com>
2014-09-08 19:25:10 +0200

    refactoring cartella

--- meshwork/doc.md
+++
@@ -1,9 +0,0 @@
-# Documentation references
-
-## D3.js Virtual Selections - enter, exit, update
-
-| page | url |
-| ---- | --- |
-| Thinking with Joins | http://bost.ocks.org/mike/join/ |
-| General update pattern example | http://bl.ocks.org/mbostock/3808218 |
-| multiline example with <code>.selectAll().data().enter()</code> | http://codepen.io/brantwills/pen/igsoc/ |
---
+++ meshwork/meshwork.js
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * Javascript for [library name]. 
+ */
+
+(function($) {
+
+  /**
+   * Adds library to the global d3 object.
+   *
+   * @param select
+   * @param settings
+   *   Array of values passed to d3_draw. 
+   *   id: required. This will be needed to attach your 
+   *       visualization to the DOM.
+   */
+  Drupal.d3.meshwork = function (select, settings) {
+    // Your custom JS.
+    
+  }
+
+})(jQuery);
