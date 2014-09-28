2838f89868d61d4c03d06448cda9e82a0bdd9df8
miromarchi <miro.marchi@gmail.com>
2014-09-28 11:36:26 +0200

    sticky drag, zoom

--- d3.network/network.js
+++ d3.network/network.js
@@ -1,13 +1,29 @@
-
-var distanceFromAge;
 (function($) {
   Drupal.d3.network = function (select, settings) {
-//    var width   = (settings.config.width || 300),
-    var network_width = 	$("#graphapi-default").width();
-//        height  = (settings.config.height || 300),
-    var network_height  = Math.max($(window).height()*0.65,320);
+    var network_width = $("#graphapi-default").width();
+//    (settings.config.height || 
+    var network_height;
+    if ($("#graphapi-default").width() > 360)
+        network_height = Math.max($(window).height()*0.65,320);
+    else
+        network_height = settings.config.height;
 
     var now=new Date();
+    
+    function msToDays(ms)
+    {
+	    return ms/(1000.0*60.0*60.0*24.0);
+    }
+
+    function daysToMs(days)
+    {
+	    return days*1000.0*60.0*60.0*24.0;
+    }
+
+    function dateValid(d)
+    {
+	    return !isNaN(d.getTime());
+    };
     
     function setStartEnd(object, timefield)
 	{
@@ -40,15 +56,14 @@ var distanceFromAge;
 			object.age=now.getTime()-object.end.getTime();
 		}
 	}
- 
     
     var maxDays=200;	
-	var min=70;
-	var max=300;
+	var min=70; /* Min and max distance */
+	var max=260;
 	var b=Math.E;
 	var k=daysToMs(maxDays)/Math.log(max-min);
 
-	distanceFromAge=function(age){
+	var distanceFromAge=function(age){
     	var v=Math.pow(b,age/k)+min;
     	v=Math.min(max,Math.max(min,v));
     	return v;
@@ -58,17 +73,15 @@ var distanceFromAge;
     	return distanceFromAge(link.age);
     }
     
-    var network_nodes   = settings.nodes,
-        network_links   = settings.links;
+    var network_nodes = settings.nodes,
+        network_links = settings.links;
 
-    // Add an attribute to each node that is a source node so that we can
-    // use that attribute to style them differently.
+    // Add an attribute to each node that is a source node so that we can use that attribute to style them differently.
     network_links.map(function(d) { network_nodes[d.target].is_source = true; });
 
     var network_force = d3.layout.force()
       .size([network_width-200, network_height])
       .charge(-100)
-      .distance(100)
       .friction(0.92)
       .gravity(0.01);
 
@@ -76,7 +89,6 @@ var distanceFromAge;
     	var link=network_links[i];
 		setStartEnd(link,"color");
 		setAge(link);
-//		console.log("age:"+msToDays(link.age)+" distance:"+distanceFromAge(link.age))
 	}
 	
     network_force.linkDistance(distanceForLink)
@@ -85,17 +97,28 @@ var distanceFromAge;
 		var node=network_nodes[i];
 		setStartEnd(node,"content");
 		setAge(node);		
-		console.log("age:"+msToDays(node.age))
 	}
 
+    var zoomListener = d3.behavior.zoom()
+        .scaleExtent([-10, 10])
+        .on("zoom", zoomHandler);
+    
+    function zoomHandler() {
+        network_graph.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    }
 
     var network_svg = d3.select('#' + settings.id).append("svg")
         .attr("width", network_width)
-        .attr("height", network_height);
+        .attr("height", network_height)
+        .call(zoomListener);
 
-    // guide per visualizzare il centro del div
-    network_svg.append("line").attr("x1",network_width/2).attr("x2",network_width/2).attr("y1",0).attr("y2",network_height).attr("stroke", "red");
-    network_svg.append("line").attr("y1",network_height/2).attr("y2",network_height/2).attr("x1",0).attr("x2",network_width).attr("stroke", "red");
+    // grid
+    network_svg.append("line").attr("x1",network_width/2).attr("x2",network_width/2).attr("y1",0).attr("y2",network_height).attr("stroke", "#ccc").attr("stroke-dasharray", "2,5");
+    network_svg.append("line").attr("x1",network_width/4).attr("x2",network_width/4).attr("y1",0).attr("y2",network_height).attr("stroke", "#ccc").attr("stroke-dasharray", "2,5");
+    network_svg.append("line").attr("x1",network_width/4*3).attr("x2",network_width/4*3).attr("y1",0).attr("y2",network_height).attr("stroke", "#ccc").attr("stroke-dasharray", "2,5");
+    network_svg.append("line").attr("y1",network_height/2).attr("y2",network_height/2).attr("x1",0).attr("x2",network_width).attr("stroke", "#ccc").attr("stroke-dasharray", "2,5");
+    network_svg.append("line").attr("y1",network_height/4).attr("y2",network_height/4).attr("x1",0).attr("x2",network_width).attr("stroke", "#ccc").attr("stroke-dasharray", "2,5");
+    network_svg.append("line").attr("y1",network_height/4*3).attr("y2",network_height/4*3).attr("x1",0).attr("x2",network_width).attr("stroke", "#ccc").attr("stroke-dasharray", "2,5");
 
     var network_graph = network_svg.append("g")
         .attr("class", "data");
@@ -111,14 +134,15 @@ var distanceFromAge;
         .attr("class", "link");
 
     var opacityMax=1;
-    var opacityMin=0.4;
+    var opacityMin=0.3;
     
     function linearRelation(px,py,qx,qy,y)
     {
 	    var a=py-px*(qy-py)/(qx-px);
     	var k=(qy-py)/(qx-px);
     	var x=(y-a)/k;
-    	return x;	
+	    	xLimited=Math.min(qx,Math.max(px,x));
+    	return xLimited;	
     }
     
     var opacityForLink=function(link)
@@ -132,13 +156,21 @@ var distanceFromAge;
     }
     network_link.style("stroke-opacity",opacityForLink)
 
-
+    var forceDrag = network_force.drag()
+    .on("dragstart", forceDragStart);
+    
+    function forceDragStart(d) { 
+        d3.event.sourceEvent.stopPropagation();
+        d.fixed = true;
+        d3.select(this).classed("fixed", true);
+    }
+    
     var network_node = network_graph.selectAll("g.node")
         .data(network_nodes)
       .enter().append("svg:g")
         .attr("class", "node")
-        .call(network_force.drag);
-
+        .call(forceDrag);
+        
 	var opacityForNode=function(node)
     {
 	    var o=opacityMin;
@@ -147,6 +179,25 @@ var distanceFromAge;
     	var ageMax=daysToMs(365);
     	return linearRelation(o,ageMax,O,ageMin,node.age);
     }
+
+//  These lines are useful for logging    
+//	for (var i=0;i<network_nodes.length;i++){
+//		var node=network_nodes[i];
+//		console.log("age:"+msToDays(node.age)+"; opacity:"+opacityForNode(node))
+//	}
+//    for (var i=0;i<network_links.length;i++){
+//    	var link=network_links[i];
+//		console.log("age:"+msToDays(link.age)+" distance:"+distanceFromAge(link.age)+" opacity:"+opacityForLink(link))
+//	}
+
+//    function textDxPosition(node) {
+//    console.log(node);
+//        if (node.cx >= network_width/2){ 
+//            return 10; 
+//        } else { 
+//            return -150; 
+//        }
+//    }
 
     network_node.append("svg:circle")
       .attr("class", "node")
@@ -161,19 +212,22 @@ var distanceFromAge;
         .append("svg:text")
         .attr("class", "nodetext")
         .attr("dx", 10)
-        .attr("dy", "1")
-        .text(function(d) { return (d.name).substring(0,20)+"..."/*+d.content */});
+        .attr("dy", 0)
+        .text(function(d) { return replaceHtmlSpecialChars((d.name).substring(0,20)+"...")})
+        .attr("fill-opacity", opacityForNode);
 
     network_force.on("tick", function() {
-      network_link.attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
+        network_link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
 
-      network_node.attr("cx", function(d) { return d.x; })
-          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-          .attr("cy", function(d) { return d.y; });
+        network_node.attr("cx", function(d) { return d.x; })
+            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+            .attr("cy", function(d) { return d.y; });
+            
     });
+    
 	function testDaysToMs()
 	{
 		assert(daysToMs(0)==0)
@@ -185,31 +239,6 @@ var distanceFromAge;
   }
 
 })(jQuery);
-
-function dateEquals(date1,date2)
-{
-	if (date1==null || date2==null)
-	{
-		return false;	
-	}
-	return !(date1>date2) && !(date1<date2);
-};
-
-function msToDays(ms)
-{
-	return ms/(1000.0*60.0*60.0*24.0);
-}
-
-function daysToMs(days)
-{
-	return days*1000.0*60.0*60.0*24.0;
-}
-
-
-function dateValid(d)
-{
-	return !isNaN(d.getTime());
-};
 
 function assert(condition, message) {
     if (condition) {
--- d3.network/search.js
+++ d3.network/search.js
@@ -1,14 +1,13 @@
-
 function textBoxKeyPress(e,form)
 {
 	var searchWord=document.getElementById("searchBox").value;
 	d3.selectAll(".searched").classed("searched",false);
 	if (searchWord.length<1) return;
-	console.log("---------------");
-	console.log(searchWord);
+//	console.log("---------------");
+//	console.log(searchWord);
 	
 	var re=new RegExp(searchWord,"i");
 	d3.selectAll(".node").filter(function(d){
-		return 	d.name.match(re) != null;
+		return 	(replaceHtmlSpecialChars(d.name)).match(re) != null;
 	}).classed("searched",true);
 }
