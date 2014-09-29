3c1afcb4c0995800dd87be5e8f821d5508f4eb7d
miromarchi <miro.marchi@gmail.com>
2014-09-29 10:37:09 +0200

    prova search

--- d3.network/search.js
+++ d3.network/search.js
@@ -1,12 +1,11 @@
-function textBoxKeyPress()
+function textBoxKeyPress(e,form)
 {
 	var searchWord=document.getElementById("searchBox").value;
 	d3.selectAll(".searched").classed("searched",false);
 	if (searchWord.length<1) return;
 	
 	var re=new RegExp(searchWord,"i");
-	d3.selectAll("g.node").filter(function(d){
-	console.log(d);
-		return 	(replaceHtmlSpecialChars(d.name)).match(re);
+	d3.selectAll(".node").filter(function(d){
+		return 	(replaceHtmlSpecialChars(d.name)).match(re) != null;
 	}).classed("searched",true);
 }
