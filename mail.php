258e38e7f8daaa20a6b99972207dddfc8ed5b1e6
miromarchi <miro.marchi@gmail.com>
2014-10-30 16:40:18 +0100

    update to panopoly 1.13

--- mail.php
+++
@@ -1,8 +0,0 @@
-&lt;?php<br />         
-         $to = &quot;miro.marchi@gmail.com&quot;; // REPLACE<br />
-         $subject = &quot;Test mail&quot;;<br />
-         $message = &quot;Hello! This is a simple email message.&quot;;<br />
-         $from = &quot;info@retebuonvivere.org&quot;;<br />
-         $headers = &quot;From:&quot; . $from;<br />
-         mail($to,$subject,$message,$headers);<br />
-         echo &quot;Mail Sent.&quot;;
