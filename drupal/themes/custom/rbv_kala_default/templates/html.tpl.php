e1e38037fa7b351de0c8cb37fb48dda708e9326c
Miro Marchi <miro.marchi@gmail.com>
2016-03-15 11:16:27 +0100

    added facebook js sdk initialization in html.tpl.php

--- drupal/themes/custom/rbv_kala_default/templates/html.tpl.php
+++ drupal/themes/custom/rbv_kala_default/templates/html.tpl.php
@@ -61,6 +61,17 @@
   <![endif]-->
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <!--[facebook javascript SDK initialization]-->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/it_IT/sdk.js#xfbml=1&version=v2.5&appId=1375538096079292";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    </script>
+  <!--[facebook js sdk end]-->
   <div id="skip-link">
     <a href="#main-content" class="element-focusable element-invisible"><?php print t('Skip to main content'); ?></a>
   </div>
